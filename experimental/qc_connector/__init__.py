0a4cd4f8f7824c29043ace23f934aa84c2149d37
João Galego <joao.galego.ext@coriant.com>
2017-04-20 15:02:19 +0100

    Added get_tree_manager and get_test_folder_factory methods

--- experimental/qc_connector/__init__.py
+++ experimental/qc_connector/__init__.py
@@ -21,10 +21,13 @@ class QCConnector(object):
     def __init__(self, url):
         """QCConnector Constructor
         :param url quality center server URL"""
+        assert url, "QC Server URL is not defined"
         self.url = url
-        self.username = ""
-        self.domain = ""
-        self.project = ""
+        self.username = None
+        self.domain = None
+        self.project = None
+        self.tree_manager = None
+        self.test_folder_factory = None
         logging.info("Initializing connection to %s", url)
         self.quality_center = Dispatch("TDApiOle80.TDConnection")
         self.quality_center.InitConnection(url)
@@ -34,6 +37,8 @@ class QCConnector(object):
         """Login to quality center server
         :param username client username
         :param password client password"""
+        assert username, "Username is not defined"
+        assert password, "Password is not defined"
         self.username = username
         self.quality_center.Login(username, password)
         logging.info("User %s is logged in", username)
@@ -42,12 +47,14 @@ class QCConnector(object):
         """Connect to the quality center server
         :param domain project domain
         :param project project name"""
+        assert domain, "Project domain is not defined"
+        assert project, "Project name is not defined"
         self.domain = domain
         self.project = project
         logging.info("Connecting to %s", self.url)
         logging.info("\tDomain: %s", self.domain)
         logging.info("\tProject: %s", self.project)
-        self.quality_center.Connect(project, domain)
+        self.quality_center.Connect(domain, project)
         logging.info("User %s is connected", self.username)
 
     def login_and_connect(self, username, password, domain, project):
@@ -57,7 +64,7 @@ class QCConnector(object):
         :param domain project domain
         :param project project name"""
         self.login(username, password)
-        self.connect(project, domain)
+        self.connect(domain, project)
 
     def logout(self):
         """Logout from the quality center server"""
@@ -92,6 +99,50 @@ class QCConnector(object):
         logging.info("Resetting project")
         self.project = None
         self.__init__(self.url)
+
+    def get_tree_manager(self):
+        """Tree Manager"""
+        logging.info("Initializing QC Tree Manager")
+        self.tree_manager = self.quality_center.TreeManager
+        return self.tree_manager
+
+    def get_test_folder_factory(self):
+        """Test Folder Factory"""
+        logging.info("Initializing QC Test Folder Factory")
+        self.test_folder_factory = self.quality_center.TestFolderFactory
+        return self.test_folder_factory
+
+    def get_node_by_path(self, node_path):
+        """Get Node By Path
+        :param node_path quality center node path"""
+        assert node_path, "Node path is not defined"
+        if self.tree_manager:
+            logging.info("Accessing node %s", node_path)
+            return self.tree_manager.NodeByPath(node_path)
+        else:
+            logging.error("No tree manager defined.")
+            logging.error("Call method QCConnector.get_tree_manager")
+            return None
+
+    def get_filtered_children(self, father_id, filt):
+        """Get Filtered Children
+        :param father_id parent identified
+        :param filt children filter"""
+        assert father_id, "Father Node ID is not defined"
+        assert filt, "Children filter is not defined"
+        if self.test_folder_factory:
+            logging.info("Filtering %s child nodes", father_id)
+            return self.test_folder_factory.GetFilteredChildren(father_id, filt)
+        else:
+            logging.error("No test node factory defined.")
+            logging.error("Call method QCConnector.get_test_folder_factory")
+            return None
+
+    def get_children_count(self, node_path):
+        """Get child count
+        :param node_path parent node path"""
+        parent_node = self.get_node_by_path(node_path)
+        print parent_node.Count
 
 class MLStripper(HTMLParser):
     """MLStripper Class
